06bc3f6286bcafe06e5e0957aeac9fdcf59e60bb
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-03 01:59:46 +0900

    DB接続のための記述

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './db/entity/User'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'devlopment',
+      password: 'pass',
+      database: 'app-db',
+      synchronize: true,
+      entities: [User],
+      migrations: ['./src/db/migration/*.ts'],
+      cli: {
+        entitiesDir: './src/db/entity',
+        migrationsDir: './src/db/migration'
+      }
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
+//
