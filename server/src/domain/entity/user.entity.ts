cfc17d59cc126d9e2c1a57170d4da531b7d79b78
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-04 15:58:39 +0900

    userのカラム構成変更

--- server/src/domain/entity/user.entity.ts
+++ server/src/domain/entity/user.entity.ts
@@ -10,8 +10,10 @@ import { Product } from './product.entity'
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string
-  @Column()
-  name: string
+  @Column({ unique: true })
+  email: string
+  @Column('varchar')
+  password: string
   @Column('timestamp')
   created_at: Date
 
