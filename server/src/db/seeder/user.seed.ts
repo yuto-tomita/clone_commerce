fe84acce278fe6b66ef1b2b147f8b9e25ef3ddc3
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-07 00:11:44 +0900

    productを生成する記述を追加

--- server/src/db/seeder/user.seed.ts
+++ server/src/db/seeder/user.seed.ts
@@ -1,6 +1,7 @@
 import { Connection } from 'typeorm'
 import { Factory, Seeder } from 'typeorm-seeding'
-import User from '../factory/user.factory'
+import { User, Product } from '../factory'
+import * as Faker from 'faker/locale/ja'
 
 export default class CreateUser implements Seeder {
   public async run(
@@ -10,7 +11,18 @@ export default class CreateUser implements Seeder {
     await factory(User)().createMany(10)
 
     const userRepository = connection.getRepository(User)
-    const user = new User()
     const firstUser = userRepository.findOne('017e26d8-0ee9-4e71-97d6-0ac20da07e62')
+
+    const productRepository = connection.getRepository(Product)
+    const product = new Product()
+
+    product.like_count = 0
+    product.product_description = Faker.commerce.productDescription()
+    product.product_images = [Faker.image.imageUrl()]
+    product.product_name = Faker.commerce.productName()
+    product.tag = [Faker.commerce.product(), Faker.commerce.product()]
+    product.user = await firstUser
+
+    await productRepository.save(product)
   }
 }
