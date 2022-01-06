664e7d1fc07d5edece5a39cc79daf08aaaf25b60
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-06 23:56:32 +0900

    userObjectを取得する文を記述

--- server/src/db/seeder/user.seed.ts
+++ server/src/db/seeder/user.seed.ts
@@ -8,5 +8,9 @@ export default class CreateUser implements Seeder {
     connection: Connection
   ): Promise<any> {
     await factory(User)().createMany(10)
+
+    const userRepository = connection.getRepository(User)
+    const user = new User()
+    const firstUser = userRepository.findOne('017e26d8-0ee9-4e71-97d6-0ac20da07e62')
   }
 }
