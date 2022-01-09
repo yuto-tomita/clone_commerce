04994dbbb5bb7ae223da43ce09f45636bb4a5f88
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-09 18:36:56 +0900

    UserModuleにentityを配置

---
+++ server/src/modules/user/entity/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+import { Product } from '@/domain/entity'
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+  @Column({ unique: true })
+  email: string
+  @Column('varchar')
+  password: string
+  @Column()
+  name: string
+  @Column({ nullable: true })
+  avatar_url: string
+  @CreateDateColumn()
+  created_at: Date
+  @UpdateDateColumn()
+  updated_at: Date
+
+  @OneToMany(() => Product, (product) => product.user)
+  products: Product[]
+}
