d744036183dc8eef70c4aa379c21eef8eec14bd0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-10 13:43:11 +0900

    DBと接続し、Repositoryパターンが使用できるように加筆

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -1,1 +1,11 @@
 import { Module } from '@nestjs/common'
+import { UsersController } from './users.controller'
+import { UsersServise } from './users.servise'
+import { usersProviders } from './providers/users.providers'
+import { DbModule } from '@db/db.module'
+@Module({
+  imports: [DbModule],
+  controllers: [UsersController],
+  providers: [...usersProviders, UsersServise]
+})
+export class UsersModule {}
--- server/src/modules/users/users.servise.ts
+++ server/src/modules/users/users.servise.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Inject } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { User } from './entity/users.entity'
 
 @Injectable()
-export class UserServise {
+export class UsersServise {
   constructor(
-    @InjectRepository(User)
+    @Inject('USER_REPOSITORY')
     private usersRepository: Repository<User>
   ) {}
 
   create(user: User) {
     this.usersRepository.create(user)
+  }
+
+  getHello() {
+    return 'hello world!'
   }
 }
