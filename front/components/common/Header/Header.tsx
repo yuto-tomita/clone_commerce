a3d531c2268c04c727aacbc9e0f119e77652721e
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-14 23:41:48 +0900

    Headerコンポーネントを仮作成し、Layoutに反映

---
+++ front/components/common/Header/Header.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Header: FC = () => {
+  return <>Headerです</>
+}
+
+export default Header
--- front/components/common/Layout/Layout.tsx
+++ front/components/common/Layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
+import { Header } from '@components/common'
 
 const Layout: FC = ({ children }) => {
   return (
     <>
+      <Header />
       <main>{children}</main>
     </>
   )
