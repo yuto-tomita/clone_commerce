b2526e425c74e4aebbf741fa477c4f58269c3e6b
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-17 21:30:38 +0900

    Inputコンポーネントを記述

--- front/components/ui/Input/Input.tsx
+++ front/components/ui/Input/Input.tsx
@@ -1,7 +1,24 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 
-const Input: FC = () => {
-  return <input type="text" />
+interface Props {
+  placeholder: string
+  bgColor: string
+  icon?: ReactNode
+}
+
+const Input: FC<Props> = ({ placeholder, bgColor, icon }) => {
+  return (
+    <div>
+      <div className="relative">
+        <input
+          type="text"
+          placeholder={placeholder}
+          className={`${bgColor} text-xs w-full focus:outline-none focus:border focus:border-blue-400 rounded-md border border-white px-7 py-2 relative`}
+        />
+      </div>
+      <div className="absolute top-5 left-32">{icon}</div>
+    </div>
+  )
 }
 
 export default Input
