ec1cbd7b8eac7193a56082517b59157e85f81068
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-19 10:10:30 +0900

    Footerを作成

--- front/components/common/Footer/Footer.tsx
+++ front/components/common/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
 const Footer: FC = () => {
-  return <>Footer</>
+  return (
+    <footer className="mt-auto h-16 bg-gray-50">
+      <div className="text-center">created_by: yuto-tomita</div>
+    </footer>
+  )
 }
 
 export default Footer
