a5beb89bd73205c5a5fb83f6c69374a5abc7c5bc
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-20 09:41:19 +0900

    任意のCSSを当てられるように加筆

--- front/components/ui/Input/Input.tsx
+++ front/components/ui/Input/Input.tsx
@@ -4,19 +4,23 @@ interface Props {
   placeholder: string
   bgColor: string
   icon?: ReactNode
+  className?: string
 }
 
-const Input: FC<Props> = ({ placeholder, bgColor, icon }) => {
+const Input: FC<Props> = ({
+  placeholder,
+  bgColor,
+  icon,
+  className
+}) => {
   return (
-    <div>
-      <div className="relative">
-        <input
-          type="text"
-          placeholder={placeholder}
-          className={`${bgColor} text-xs w-full focus:outline-none focus:border focus:border-blue-400 rounded-md border border-white px-7 py-2 relative`}
-        />
-      </div>
-      <div className="absolute top-5 left-32">{icon}</div>
+    <div className="relative">
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={`${className} ${bgColor} text-xs w-full focus:outline-none focus:border focus:border-blue-400 rounded-md border border-white px-7 py-3 relative`}
+      />
+      <div className="absolute top-3 left-8">{icon}</div>
     </div>
   )
 }
