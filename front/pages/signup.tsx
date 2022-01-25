876adb57676d0204dcfb7492ebbbf34671caf8be
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-25 21:11:16 +0900

    会員作成ページのモック作成

--- front/pages/signup.tsx
+++ front/pages/signup.tsx
@@ -1,11 +1,11 @@
-import { Text, Input, Radio } from '@components/ui'
+import { Text, Input, Radio, Button } from '@components/ui'
 import style from '@styles/Signup.module.css'
 import { RADIO_OPTION } from 'lib/constant/SignupPageConstant'
 import { useState } from 'react'
 
 export default function Signup() {
   const [mail, setMail] = useState('')
-  const [password, setPassword] = useState('gheo')
+  const [password, setPassword] = useState('')
   const [userName, setUserName] = useState('')
   const [gender, setGender] = useState<null | number>(null)
 
@@ -51,8 +51,11 @@ export default function Signup() {
           onChange={setGender}
           defaultValue={gender}
         />
+
+        <Button variant="primary" className="ml-auto">
+          送信
+        </Button>
       </div>
-      {setGender}
     </div>
   )
 }
