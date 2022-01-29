dfd776dcf2617f90913411db808e72d836982658
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-01-30 05:50:52 +0900

    メールアドレスのバリデーションチェックでエラーメッセージを格納するように加筆

--- front/pages/signup.tsx
+++ front/pages/signup.tsx
@@ -2,15 +2,63 @@ import { Text, Input, Radio, Button } from '@components/ui'
 import style from '@styles/Signup.module.css'
 import { RADIO_OPTION } from 'lib/constant/SignupPageConstant'
 import { useState } from 'react'
+import { useValidation } from 'lib/hooks/useValidation'
+
+interface ValidationError {
+  mail: string
+  password: string
+  userName: string
+  gender: string
+}
 
 export default function Signup() {
   const [mail, setMail] = useState('')
   const [password, setPassword] = useState('')
   const [userName, setUserName] = useState('')
   const [gender, setGender] = useState<null | number>(null)
+  const [error, setError] = useState<ValidationError>({
+    mail: '',
+    password: '',
+    userName: '',
+    gender: ''
+  })
+  const { required, isMail } = useValidation()
+
+  const submitToServer = () => {
+    initialErrorState()
+
+    if (!required(mail)) {
+      setError({ ...error, mail: 'メールアドレスを入力してください' })
+    } else {
+      if (!isMail(mail)) {
+        // errorStateに複数の状態を保持するために関数型でStateに値を保存する
+        setError((error) => {
+          return {
+            ...error,
+            mail: 'メールアドレスの形式で入力してください'
+          }
+        })
+      }
+    }
+  }
+
+  const initialErrorState = () => {
+    setError((prevState) => {
+      return {
+        ...prevState,
+        mail: '',
+        password: '',
+        userName: '',
+        gender: ''
+      }
+    })
+  }
 
   return (
     <div>
+      {Object.values(error).map((val, key) => (
+        <li key={key}>{val}</li>
+      ))}
       <Text className="text-center" variant="sectionHeading">
         会員登録
       </Text>
@@ -47,12 +95,16 @@ export default function Signup() {
         <label>性別</label>
         <Radio
           className="flex"
-          radioButtonData={RADIO_OPTION}
+          options={RADIO_OPTION}
           onChange={setGender}
           defaultValue={gender}
         />
 
-        <Button variant="primary" className="ml-auto">
+        <Button
+          variant="primary"
+          className="ml-auto"
+          onClick={submitToServer}
+        >
           送信
         </Button>
       </div>
