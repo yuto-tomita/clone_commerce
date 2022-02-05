36a2f530de34055433330b1f7ccdc3dd003a6727
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-02-05 21:43:59 +0900

    storeを使用するためにProviderを追加

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '@components/common'
+import { Provider } from 'react-redux'
+import { store } from 'store/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
-        <Component {...pageProps} />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
       </Layout>
     </>
   )
