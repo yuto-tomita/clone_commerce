a0ba108215f033f489a8967c4c32c26753398f9c
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-02-09 23:37:47 +0900

    cors対策

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET, POST, PUT, DELETE'
+  })
   await app.listen(8080)
 }
 bootstrap()
