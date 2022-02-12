f3246fb4ca81db238e924cfb8d99f76ae4839f72
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-02-12 21:02:41 +0900

    swaggerを作成するためのライブラリを導入

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -9,6 +10,14 @@ async function bootstrap() {
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     methods: 'GET, POST, PUT, DELETE'
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('e-commerce App RESTAPI Document')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   await app.listen(8080)
 }
 bootstrap()
