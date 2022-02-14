df25da3ccb7ab2989e3d918dd86dddda07cdd807
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-02-14 20:15:01 +0900

    class-validatorを使用してバリデーションルールを加筆

--- server/src/modules/users/dto/users.dto.ts
+++ server/src/modules/users/dto/users.dto.ts
@@ -1,8 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { GenderEnum } from '../entity/users.entity'
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator'
+
 export class CreateUserDto {
-	readonly email: string
-	readonly password: string
-	readonly name: string
-	readonly gender: number
-	readonly avatar_image?: string
-	readonly profile_text?: string
+  @ApiProperty()
+  @IsNotEmpty({ message: 'メールアドレスを入力してください' })
+  @IsEmail({ message: 'メールアドレスの形式で入力してください' })
+  email: string
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'パスワードを入力してください' })
+  password: string
+
+  @ApiProperty()
+  @IsNotEmpty({ message: '名前を入力してください' })
+  readonly name: string
+
+  @ApiProperty({ enum: GenderEnum })
+  @IsNotEmpty({ message: '性別を選択してください' })
+  @IsEnum(GenderEnum, { message: '選択されている値が不正です' })
+  readonly gender: GenderEnum
+
+  @ApiProperty()
+  readonly avatar_image?: string
+
+  @ApiProperty()
+  readonly profile_text?: string
 }
